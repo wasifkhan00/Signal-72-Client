c43056f6c95d82c51f293bf87e35b24462d4705e
wasifkhan00 <ukhanwasif@gmail.com>
2025-10-03 21:53:11 +0500

    fixed home page redirection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,24 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import Chat from "@components/Chat";
+import { AuthStore } from "@store/AuthStore";
 
 export default function Home() {
   const router = useRouter();
+  const { showChat } = AuthStore();
+  const [isCheckingAuth, setIsCheckingAuth] = useState(true); // local state to wait
 
-  // useEffect(() => {
-  //   router.push("/login");
-  // }, [router]);
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (!token || !showChat) {
+      router.replace("/login");
+    } else {
+      setIsCheckingAuth(false); // show chat only if valid
+    }
+  }, [showChat]);
 
   return null;
 }
